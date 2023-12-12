e999bbd859d1be3a63996677758c0e78b6b3ada5
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-12-12 10:54:29 +0000

    Deploying to gh-pages from @ Yurtov/cars-rent@f8fbc8336288e9a5d749192fcd17ea401ac16198 🚀

--- static/js/681.e427a11b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcars_rent=self.webpackChunkcars_rent||[]).push([[681],{9681:function(e,r,n){n.r(r);var s=n(184);r.default=function(){return(0,s.jsx)("div",{children:"Favorites"})}}}]);
-//# sourceMappingURL=681.e427a11b.chunk.js.map
